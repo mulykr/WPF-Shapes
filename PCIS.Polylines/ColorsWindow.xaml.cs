83161b749ec8eaba3549b42a6d3598fc8c7773e0
Roman Mulyk <work.mulykr@gmail.com>
2018-10-21 21:46:34 +0300

    ColorsWindow added

---
+++ PCIS.Polylines/ColorsWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Polylines
+{
+    public partial class ColorsWindow : Window
+    {
+        public ColorsWindow(MainViewModel mainViewModel)
+        {
+            InitializeComponent();
+            DataContext = mainViewModel;
+        }
+    }
+}
